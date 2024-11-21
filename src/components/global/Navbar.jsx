cfb4bd08aaba8529367c221b6326d4420b9c3fe0
Ali Anani <aliananii120@gmail.com>
2024-11-21 20:05:14 +0300

    First Commit - Initialized Jobster Project

---
+++ src/components/global/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between items-center my-2 px-4'>
+      {/* Logo */}
+      <div>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
